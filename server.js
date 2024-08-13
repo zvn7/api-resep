1e6cc731b1ae6620c479e47d5417405b65f98c72
Ilham Maulana <aammaul7@gmail.com>
2024-08-13 07:16:07 +0700

    [fixed] fix cors origin problem

--- server.js
+++ server.js
@@ -1,24 +1,33 @@
 import express from "express";
 import dotenv from "dotenv";
-import ConnectDB from "./config/db.js"
-import AuthRouter from "./routes/AuthRoutes.js"
-import ResepRouter from "./routes/ResepRoutes.js"
-import UserRouter from "./routes/UserRoutes.js"
-import CommentRouter from "./routes/CommentRoutes.js"
+import ConnectDB from "./config/db.js";
+import AuthRouter from "./routes/AuthRoutes.js";
+import ResepRouter from "./routes/ResepRoutes.js";
+import UserRouter from "./routes/UserRoutes.js";
+import CommentRouter from "./routes/CommentRoutes.js";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 app.use(express.json());
 
+app.use(
+	cors({
+		origin: "*",
+		methods: ["GET", "POST", "PUT", "DELETE"],
+		credentials: true,
+	})
+);
+
 // route
-app.use('/api/v1/auth', AuthRouter);
-app.use('/api/v1/', ResepRouter);
-app.use('/api/v1/', UserRouter);
-app.use('/api/v1/', CommentRouter);
+app.use("/api/v1/auth", AuthRouter);
+app.use("/api/v1/", ResepRouter);
+app.use("/api/v1/", UserRouter);
+app.use("/api/v1/", CommentRouter);
 
 // connect db
-ConnectDB()
+ConnectDB();
 
 // port
 const port = process.env.PORT;
